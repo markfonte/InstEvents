ddbbf717abbe47cb6d7b22d543faa10149758597
Mark Fonte <mark@fonte.com>
2019-10-12 07:10:24 -0400

    added bottom navigation

--- app/src/main/java/example/com/eventmap/MapsActivity.kt
+++ app/src/main/java/example/com/eventmap/MapsActivity.kt
@@ -6,6 +6,8 @@ import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import example.com.eventmap.databinding.ActivityMapsBinding
 import example.com.eventmap.util.InjectorUtils
 
@@ -16,22 +18,29 @@ class MapsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setTheme(R.style.Theme_Design_Light_NoActionBar)
         val factory: MapsActivityViewModelFactory =
             InjectorUtils.provideMapsActivityViewModelFactory()
         vm = ViewModelProviders.of(this, factory).get(MapsActivityViewModel::class.java)
-        val binding: ActivityMapsBinding =
-            DataBindingUtil.setContentView<ActivityMapsBinding>(this, R.layout.activity_maps)
-                .apply {
-                    viewModel = vm
-                    lifecycleOwner = this@MapsActivity
-                }
+        DataBindingUtil.setContentView<ActivityMapsBinding>(this, R.layout.activity_maps)
+            .apply {
+                viewModel = vm
+                lifecycleOwner = this@MapsActivity
+            }
 
         val navHost: NavHostFragment =
             supportFragmentManager.findFragmentById(R.id.main_nav_host_fragment) as NavHostFragment?
                 ?: return
 
         navController = navHost.navController
+
+        initializeBottomNav(navController)
+    }
+
+    private fun initializeBottomNav(navController: NavController) {
+        findViewById<BottomNavigationView>(R.id.bottom_nav)?.let { bottomNavigationView ->
+            NavigationUI.setupWithNavController(bottomNavigationView, navController)
+        }
     }
 
 
