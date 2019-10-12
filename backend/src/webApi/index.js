8a1fccd71dffc6e8134f9fdd70680ebe8cf79658
Nathaniel Johnson <nathancj224@gmail.com>
2019-10-12 15:42:07 -0400

    Move functions code up into backend directory

--- backend/src/webApi/index.js
+++ backend/src/webApi/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const db = require('../db');
+const parseDate = require('../util/parseDate');
 
 app.use(cors({ origin: true }));
 
